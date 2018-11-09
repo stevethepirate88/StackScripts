02dd756eee4f9c8be0bbcae54326daeae34d48d3
stevethepirate88 <stephen.crane.umd@gmail.com>
2018-11-09 12:41:32 -0500

    trying something to with dependencies
    Trying something with dependencies since some of the dependencies aren't working.

--- LinuxGSM_Hardened.sh
+++ LinuxGSM_Hardened.sh
@@ -82,8 +82,7 @@ echo "$GAMESERVER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 # Install LinuxGSM and the Game Server of your choice
 echo Setting up Dependencies
-export DEBIAN_FRONTEND=noninteractive
-dependency_$GAMESERVER
+export DEBIAN_FRONTEND=noninteractive dependency_$GAMESERVER
 
 #
 # Continuing with download, installation, setup, and execution of the game server
